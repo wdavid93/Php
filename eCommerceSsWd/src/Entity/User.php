3e720800916e25996f99eee2d1c10afd61e24ad4
William David <william.david93000@gmail.com>
2024-07-15 23:11:46 +0200

    database and migration

--- eCommerceSsWd/src/Entity/User.php
+++ eCommerceSsWd/src/Entity/User.php
@@ -1,53 +1,63 @@
 <?php
 
+// Déclaration du namespace pour la classe User
 namespace App\Entity;
 
+// Importation des classes nécessaires
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-#[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`user`')]
-#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+// Déclaration de la classe User avec les attributs de Doctrine ORM
+#[ORM\Entity(repositoryClass: UserRepository::class)] // Spécifie que cette entité utilise UserRepository
+#[ORM\Table(name: '`user`')] // Déclare le nom de la table 'user' en échappant les caractères réservés
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])] // Ajoute une contrainte unique sur la colonne 'email'
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    // Déclaration de l'ID de l'utilisateur
+    #[ORM\Id] // Spécifie que c'est la clé primaire
+    #[ORM\GeneratedValue] // Indique que la valeur est auto-générée
+    #[ORM\Column] // Indique que c'est une colonne de la table
+    private ?int $id = null; // Déclaration de la variable $id comme un entier nullable
 
-    #[ORM\Column(length: 180)]
-    private ?string $email = null;
-
-    /**
-     * @var list<string> The user roles
-     */
-    #[ORM\Column]
-    private array $roles = [];
+    // Déclaration de l'email de l'utilisateur
+    #[ORM\Column(length: 180)] // Indique que c'est une colonne de la table avec une longueur maximale de 180 caractères
+    private ?string $email = null; // Déclaration de la variable $email comme une chaîne nullable
 
     /**
-     * @var string The hashed password
+     * @var list<string> Les rôles de l'utilisateur
      */
-    #[ORM\Column]
-    private ?string $password = null;
+    #[ORM\Column] // Indique que c'est une colonne de la table
+    private array $roles = []; // Déclaration de la variable $roles comme un tableau
 
-    #[ORM\Column(length: 255)]
-    private ?string $name = null;
+    /**
+     * @var string Le mot de passe haché
+     */
+    #[ORM\Column] // Indique que c'est une colonne de la table
+    private ?string $password = null; // Déclaration de la variable $password comme une chaîne nullable
 
-    #[ORM\Column(length: 255)]
-    private ?string $firstname = null;
+    // Déclaration du nom de l'utilisateur
+    #[ORM\Column(length: 255)] // Indique que c'est une colonne de la table avec une longueur maximale de 255 caractères
+    private ?string $name = null; // Déclaration de la variable $name comme une chaîne nullable
 
+    // Déclaration du prénom de l'utilisateur
+    #[ORM\Column(length: 255)] // Indique que c'est une colonne de la table avec une longueur maximale de 255 caractères
+    private ?string $firstname = null; // Déclaration de la variable $firstname comme une chaîne nullable
+
+    // Méthode pour obtenir l'ID de l'utilisateur
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    // Méthode pour obtenir l'email de l'utilisateur
     public function getEmail(): ?string
     {
         return $this->email;
     }
 
+    // Méthode pour définir l'email de l'utilisateur
     public function setEmail(string $email): static
     {
         $this->email = $email;
@@ -56,13 +66,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     }
 
     /**
-     * A visual identifier that represents this user.
+     * Identifiant visuel qui représente cet utilisateur.
      *
      * @see UserInterface
      */
     public function getUserIdentifier(): string
     {
-        return (string) $this->email;
+        return (string) $this->email; // Utilise l'email comme identifiant de l'utilisateur
     }
 
     /**
@@ -73,10 +83,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
+        // Garantie que chaque utilisateur a au moins le rôle 'ROLE_USER'
         $roles[] = 'ROLE_USER';
 
-        return array_unique($roles);
+        return array_unique($roles); // Retourne les rôles uniques
     }
 
     /**
@@ -94,9 +104,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getPassword(): string
     {
-        return $this->password;
+        return $this->password; // Retourne le mot de passe de l'utilisateur
     }
 
+    // Méthode pour définir le mot de passe de l'utilisateur
     public function setPassword(string $password): static
     {
         $this->password = $password;
@@ -109,15 +120,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function eraseCredentials(): void
     {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+        // Si vous stockez des données sensibles temporaires sur l'utilisateur, nettoyez-les ici
+        // $this->plainPassword = null; // Exemple de nettoyage de données sensibles
     }
 
+    // Méthode pour obtenir le nom de l'utilisateur
     public function getName(): ?string
     {
         return $this->name;
     }
 
+    // Méthode pour définir le nom de l'utilisateur
     public function setName(string $name): static
     {
         $this->name = $name;
@@ -125,11 +138,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+    // Méthode pour obtenir le prénom de l'utilisateur
     public function getFirstname(): ?string
     {
         return $this->firstname;
     }
 
+    // Méthode pour définir le prénom de l'utilisateur
     public function setFirstname(string $firstname): static
     {
         $this->firstname = $firstname;
--- eCommerceSsWd/src/Repository/UserRepository.php
+++ eCommerceSsWd/src/Repository/UserRepository.php
@@ -1,7 +1,9 @@
 <?php
 
+// Déclaration du namespace pour la classe UserRepository
 namespace App\Repository;
 
+// Importation des classes nécessaires
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -10,51 +12,68 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 
 /**
+ * Cette classe étend ServiceEntityRepository et implémente PasswordUpgraderInterface
+ * pour gérer les utilisateurs et mettre à jour leurs mots de passe.
+ *
  * @extends ServiceEntityRepository<User>
  */
 class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
+    // Constructeur de la classe UserRepository
     public function __construct(ManagerRegistry $registry)
     {
+        // Appel du constructeur parent avec le registre de gestion et la classe User
         parent::__construct($registry, User::class);
     }
 
     /**
-     * Used to upgrade (rehash) the user's password automatically over time.
+     * Méthode utilisée pour mettre à jour (rehacher) le mot de passe de l'utilisateur automatiquement au fil du temps.
+     *
+     * @param PasswordAuthenticatedUserInterface $user L'utilisateur dont le mot de passe doit être mis à jour
+     * @param string $newHashedPassword Le nouveau mot de passe haché
      */
     public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
     {
+        // Vérification si l'utilisateur est une instance de la classe User
         if (!$user instanceof User) {
+            // Si ce n'est pas le cas, lever une exception UnsupportedUserException
             throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', $user::class));
         }
 
+        // Mise à jour du mot de passe de l'utilisateur
         $user->setPassword($newHashedPassword);
+        // Persistance de l'utilisateur avec le nouveau mot de passe dans la base de données
         $this->getEntityManager()->persist($user);
+        // Validation des changements dans la base de données
         $this->getEntityManager()->flush();
     }
 
-    //    /**
-    //     * @return User[] Returns an array of User objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    // Méthodes de recherche commentées pour un usage futur potentiel
 
-    //    public function findOneBySomeField($value): ?User
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    // /**
+    //  * @return User[] Returns an array of User objects
+    //  */
+    // public function findByExampleField($value): array
+    // {
+    //     // Création d'un constructeur de requêtes pour trouver des utilisateurs par un champ d'exemple
+    //     return $this->createQueryBuilder('u')
+    //         ->andWhere('u.exampleField = :val')
+    //         ->setParameter('val', $value)
+    //         ->orderBy('u.id', 'ASC')
+    //         ->setMaxResults(10)
+    //         ->getQuery()
+    //         ->getResult()
+    //     ;
+    // }
+
+    // public function findOneBySomeField($value): ?User
+    // {
+    //     // Création d'un constructeur de requêtes pour trouver un utilisateur par un champ d'exemple
+    //     return $this->createQueryBuilder('u')
+    //         ->andWhere('u.exampleField = :val')
+    //         ->setParameter('val', $value)
+    //         ->getQuery()
+    //         ->getOneOrNullResult()
+    //     ;
+    // }
 }
